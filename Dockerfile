263a397d0340982edc98a5be485548b96084c77f
davidgodinez <davidgodinez70@gmail.com>
2022-10-05 07:20:48 -0500

    Update to working Dockerfile.

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,9 @@
 FROM python:3.9 
-# Or any preferred Python version.
-RUN apt-get update
-RUN apt-get install libasound-dev libportaudio2 libportaudiocpp0 portaudio19-dev -y
-RUN pip install pyaudio
-RUN /usr/local/bin/python -m pip install --upgrade pip
-COPY . bible-app/
+RUN addgroup app && useradd -r -g app appuser
+# RUN apt-get update && apt install -y python3-gi python3-gi-cairo gir1.2-gtk-3.0
+RUN apt-get update && apt install -y libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-3.0 portaudio19-dev 
 WORKDIR  /bible-app
-RUN pip install -r ./requirements3.txt
-CMD [“python”, “./app.py”] 
-# Or enter the name of your unique directory and parameter set.
+COPY . .
+RUN pip install -r requirements.txt
+USER appuser
+CMD ["python", "./bible/app2.py"]
---
+++ bible/app2.py
@@ -0,0 +1,62 @@
+import os
+import pythonbible as bible
+from gtts import gTTS
+from playsound import playsound
+import speech_recognition as sr 
+from pydub.silence import split_on_silence
+
+#create docker file to test out 
+# TODO need to account for ranges of bible verses, can use regex splicing to do this
+
+# os.chdir('./bible')
+
+
+# def welcome():
+#     playsound('./welcome.mp3')
+
+
+def main():
+#    welcome()
+
+    # Speech input
+    r = sr.Recognizer()
+    with sr.Microphone(device_index=0,sample_rate=44100) as source:
+        # record the audio data from the default microphone
+        print("Recognizing...")
+        r.adjust_for_ambient_noise(source)
+        audio_data = r.record(source, duration=5)
+        print('Done')
+        # convert speech to text
+        user_input = r.recognize_google(audio_data)
+        print(user_input)
+
+    normalized_reference = bible.normalize_reference(user_input)
+    converted_reference = bible.convert_references_to_verse_ids(normalized_reference)
+    language = 'en'
+
+    if len(converted_reference) == 1:
+        verse_text = bible.get_verse_text(converted_reference[0])
+        print(f"The verse is: \n{verse_text}")
+        my_obj = gTTS(verse_text, lang=language)
+        my_obj.save(f"{user_input}.mp3")
+        playsound(f"{user_input}.mp3")
+    elif len(converted_reference) > 1:
+        verse_list = []
+        for verse in converted_reference:
+            verse_list.append(bible.get_verse_text(verse))
+        verse_texts = ' '.join(verse_list)
+        print(f'The verses are:\n{verse_texts}')
+        my_obj = gTTS(verse_texts, lang=language)
+        my_obj.save(f"{user_input}.mp3")
+        playsound(f"{user_input}.mp3")
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
